bc2e220301d76f0de79d0d220971e984b1bee197
Robert Siński <robertbiplosek@gmail.com>
2026-03-11 22:50:00 +0100

    refactor: reorganize repository imports for cinemas, cities, and showtimes; remove obsolete wrappers and streamline MoviesRepository and MoviesService for improved clarity and maintainability

--- src/movies/movies.module.ts
+++ src/movies/movies.module.ts
@@ -3,13 +3,12 @@ import { CacheModule } from '@nestjs/cache-manager';
 import { MoviesController } from './movies.controller';
 import { MoviesService } from './movies.service';
 import { MoviesRepository } from './movies.repository';
-import { MoviesBatchService } from './movies-batch.service';
 import { GenresModule } from '../genres/genres.module';
 
 @Module({
   imports: [CacheModule.register(), GenresModule],
   controllers: [MoviesController],
-  providers: [MoviesService, MoviesRepository, MoviesBatchService],
+  providers: [MoviesService, MoviesRepository],
   exports: [MoviesService],
 })
 export class MoviesModule {}
